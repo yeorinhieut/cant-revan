077ed721854d0ae3317d4a7e98573085906cea2b
여린히읗 <damhwee@icloud.com>
2024-01-16 03:32:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import dist, find_packages, setup
 dist.Distribution().fetch_build_eggs(["Cython"])
 
 setup(
-    name="canrevan",
-    version="4.0.0",
-    author="Jungwoo Park",
-    author_email="affjljoo3581@gmail.com",
+    name="cant-revan",
+    version="5.0.0",
+    author="yeorinhieut",
+    author_email="yeorinhieut@gmail.com",
     description="Simple Naver News Crawler",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     keywords=["naver", "news", "dataset", "nlp"],
-    url="https://github.com/affjljoo3581/canrevan",
+    url="https://github.com/yeorinhieut/cant-revan",
     license="Apache-2.0",
     package_dir={"": "src"},
     packages=find_packages("src"),
